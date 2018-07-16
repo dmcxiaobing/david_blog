49665f591fa016fb8402c3190b39f864b0795e17
david <986945193@qq.com>
2018-07-16 21:23:25 +0800

    Initial commit

---
+++ src/main/java/com/david/DavidBlogApplication.java
@@ -0,0 +1,12 @@
+package com.david;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidBlogApplication.class, args);
+	}
+}
---
+++ src/main/java/com/david/entity/UserEntity.java
@@ -0,0 +1,16 @@
+package com.david.entity;
+
+import lombok.*;
+
+/**
+ * @author ：David
+ * @weibo ：http://weibo.com/mcxiaobing
+ * @github: https://github.com/QQ986945193
+ */
+//@EqualsAndHashCode
+//@ToString
+//@Setter
+//@Getter
+@Data
+public class UserEntity {
+}
